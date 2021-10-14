e860a5abfefe30bb03bfda7a60a2e265c641dbb0
alberto <alyoung1991@gmail.com>
2021-10-13 21:30:24 -0400

    Go big or go Home

--- src/scripts/score.js
+++ src/scripts/score.js
@@ -13,22 +13,34 @@ class Score {
         // if points < 0, animate jiggle and red coloring
         if(points === 100){
             if(this.scoreDisplay.classList.contains("win-point1")){
+                this.scoreDisplay.classList.remove("lose-point1");
+                this.scoreDisplay.classList.remove("lose-point2");
                 this.scoreDisplay.classList.remove("win-point1");
                 this.scoreDisplay.classList.add("win-point2");
             }else if(this.scoreDisplay.classList.contains("win-point2")){
+                this.scoreDisplay.classList.remove("lose-point1");
+                this.scoreDisplay.classList.remove("lose-point2");
                 this.scoreDisplay.classList.remove("win-point2");
                 this.scoreDisplay.classList.add("win-point1");
             }else{
+                this.scoreDisplay.classList.remove("lose-point1");
+                this.scoreDisplay.classList.remove("lose-point2");
                 this.scoreDisplay.classList.add("win-point1");
             }
         }else if(points === -100){
             if(this.scoreDisplay.classList.contains("lose-point1")){
+                this.scoreDisplay.classList.remove("win-point1");
+                this.scoreDisplay.classList.remove("win-point2");
                 this.scoreDisplay.classList.remove("lose-point1");
                 this.scoreDisplay.classList.add("lose-point2");
             }else if(this.scoreDisplay.classList.contains("lose-point2")){
+                this.scoreDisplay.classList.remove("win-point1");
+                this.scoreDisplay.classList.remove("win-point2");
                 this.scoreDisplay.classList.remove("lose-point2");
                 this.scoreDisplay.classList.add("lose-point1");
             }else{
+                this.scoreDisplay.classList.remove("win-point1");
+                this.scoreDisplay.classList.remove("win-point2");
                 this.scoreDisplay.classList.add("lose-point1");
             }
         }
